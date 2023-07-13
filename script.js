a346d9ab9bd948c3eb47ef9fbe13afba2770a92c
ummesalamaabulfazal <138029412+ummesalamaabulfazal@users.noreply.github.com>
2023-07-13 22:25:16 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// let searchform = document.querySelector('.search-form');
+// document.querySelector('#search-btn').onclick = () =>{
+//   searchform.classList.toggle('active');
+// }
+
+// let shoppingcart = document.querySelector('.shopping-cart');
+// document.querySelector('#cart-btn').onclick = () =>{
+//   shoppingcart.classList.toggle('active');
+
+let loginForm = document.querySelector('.login-form');
+document.querySelector('.bi-person-fill').onclick = () => {
+  loginForm.classList.toggle('active');
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
